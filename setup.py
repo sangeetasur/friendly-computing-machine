5476605d1e9980155ed9a40260ce183af6abbb6a
ssx9f <ssx9f@mst.edu>
2017-07-25 14:00:04 -0500

    Started test

---
+++ setup.py
@@ -0,0 +1,49 @@
+import setuptools
+
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='friendly_computing_machine',
+        version="0.1.1",
+        description='A starting template for Python programs',
+        author='Sangeeta Sur',
+        author_email='ssx9f@mst.edu',
+        url="https://github.com/ssx9f/friendly-computing-machine",
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'numpy>=1.7',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'tox',
+            ],
+        },
+
+
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+            'tox',
+        ],
+
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+        ],
+        zip_safe=True,
+    )
