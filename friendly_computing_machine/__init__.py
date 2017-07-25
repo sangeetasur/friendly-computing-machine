09570f6e390535ed7f6e0de22fc9c558c34dc5be
ssx9f <ssx9f@mst.edu>
2017-07-25 13:29:27 -0500

    Start the FCM

---
+++ friendly_computing_machine/__init__.py
@@ -0,0 +1,8 @@
+"""
+This is the base file of the friendly-computing-machine!
+"""
+
+
+from . import math
+from .math import mult
+
---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,26 @@
+"""
+A small set of functions for doing math operations.
+"""
+
+# Write a function named add that adds two values
+
+def add(arg1, arg2):
+	"""
+	
+	Function that adds two arguments
+	"""
+	return 2+5
+
+
+def mult(arg1, arg2):
+        """
+
+        Function that multiplies two arguments
+        """
+        return 2*5
+
+
+
+
+
+
